dc5517953742c1193c8d8ff8081697267303e54f
nguyen-hai-nam <nam.nn265@gmail.com>
2023-11-12 00:56:09 +0700

    Create SearchResultCard component

---
+++ src/components/SearchResultCard.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import Link from 'next/link'
+import Image from 'next/image'
+
+const SearchResultCard = (props: { name: string; bio: string; roles: string[];}) => {
+    return (
+        <Link href="/">
+            <div className="inline-block card w-80 border-2 hover:shadow-md">
+                <figure className="avatar">
+                    <div className="h-80 w-80">
+                        <Image src="/vercel.svg" width={320} height={320} alt='Profile Picture' />
+                    </div>
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">
+                        <span className='line-clamp-1'>{ props.name }</span>
+                        <div className="badge badge-secondary">PROMOTED</div>
+                    </h2>
+                    <p className="h-[72px] line-clamp-3">{ props.bio }</p>
+                    <div className="max-h-[22px] h-fit overflow-hidden card-actions justify-end">
+                        {props.roles?.map((role, index) => (
+                            <div key={index} className="badge badge-outline">{ role }</div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </Link>
+    )
+}
+
+export default SearchResultCard
