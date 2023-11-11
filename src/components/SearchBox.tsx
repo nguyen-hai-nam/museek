a6844c40afbbee1865e583db8430c68bfaf8c9da
nguyen-hai-nam <nam.nn265@gmail.com>
2023-11-12 00:55:56 +0700

    Create SearchBox component

---
+++ src/components/SearchBox.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+const SearchBox = () => {
+    return (
+        <input type="text" placeholder="Seeking ..." className="input input-bordered w-full lg:w-3/5" />
+    )
+}
+
+export default SearchBox
