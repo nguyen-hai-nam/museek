4016360e5d7c68d73f5fc92c1f85ab9ff90c7fbf
nguyen-hai-nam <nam.nn265@gmail.com>
2023-11-17 00:32:29 +0700

    Restyle profile page

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -2,44 +2,42 @@
 
 import Image from 'next/image'
 
+const profile = {
+    name: 'Nguyen Hai Nam',
+    gender: 'male',
+    birthday: new Date(),
+    phoneNumber: '0123456789',
+    email: 'museek@gmail.com',
+    location: 'Ha Noi, Viet Nam',
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    roles: ['Vocalist', 'Guitarist', 'Bassist', 'Drummer', 'Producer', 'DJ']
+}
+
 export default function Profile({ params }: { params: { id: string } }) {
     return (
         <main className="mx-auto pt-8 w-3/5">
             <div className='flex justify-between'>
-                <div className="w-1/4 avatar cursor-pointer">
+                <div className="w-1/6 avatar cursor-pointer">
                     <div className="w-full rounded-full">
                         <Image src="/vercel.svg" width={256} height={256} alt='Profile Picture' />
                     </div>
                 </div>
-                <div className='w-2/3 max-w-full'>
+                <div className='w-3/4 max-w-full'>
                     <div className='flex items-center justify-between'>
-                        <span className='text-xl font-semibold'>Nguyen Hai Nam</span>
+                        <span className='text-xl font-semibold'>{profile.name}</span>
                         <button className='btn btn-sm'>Edit profile</button>
                     </div>
                     <div className='mt-4 xl:w-4/5 grid grid-cols-2'>
-                        <p>Location: Hanoi</p>
-                        <p>Gender: Male</p>
-                        <p>Birthday: 11/10/2002</p>
+                        <p className='font-semibold'>Phone: {profile.phoneNumber}</p>
+                        <p className='font-semibold'>Email: {profile.email}</p>
+                        <p className='font-semibold'>Location: Hanoi</p>
+                        <p className='font-semibold'>Gender: {profile.gender === 'male' ? 'Male' : 'Female'}</p>
+                        <p className='font-semibold'>Birthday: {profile.birthday.toDateString()}</p>
                     </div>
-                    <div className='mt-4 xl:w-4/5 flex flex-wrap gap-1'>
-                        <div className="badge badge-outline">Vocalist</div>
-                        <div className="badge badge-outline">Guitarist</div>
-                        <div className="badge badge-outline">Bassist</div>
-                        <div className="badge badge-outline">Drummer</div>
-                        <div className="badge badge-outline">Producer</div>
-                        <div className="badge badge-outline">DJ</div>
-                        <div className="badge badge-outline">Vocalist</div>
-                        <div className="badge badge-outline">Guitarist</div>
-                        <div className="badge badge-outline">Bassist</div>
-                        <div className="badge badge-outline">Drummer</div>
-                        <div className="badge badge-outline">Producer</div>
-                        <div className="badge badge-outline">DJ</div>
-                        <div className="badge badge-outline">Vocalist</div>
-                        <div className="badge badge-outline">Guitarist</div>
-                        <div className="badge badge-outline">Bassist</div>
-                        <div className="badge badge-outline">Drummer</div>
-                        <div className="badge badge-outline">Producer</div>
-                        <div className="badge badge-outline">DJ</div>
+                    <div className='mt-4 xl:w-4/5 flex flex-wrap gap-2'>
+                        {profile.roles.map((role, index) => (
+                            <div key={index} className='badge badge-outline font-semibold border-2'>{role}</div>
+                        ))}
                     </div>
                 </div>
             </div>
