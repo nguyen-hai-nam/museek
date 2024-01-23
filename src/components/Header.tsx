fb24b1bba824d0a97dc0c7cf1c1a546dce00a198
nguyen-hai-nam <nam.nn265@gmail.com>
2024-01-23 09:37:41 +0700

    Remove collaboration page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,10 +14,6 @@ const headerItems = [
         href: '/seek'
     },
     {
-        name: 'Collaboration',
-        href: '/collaboration'
-    },
-    {
         name: 'Chat',
         href: '/chat'
     }
